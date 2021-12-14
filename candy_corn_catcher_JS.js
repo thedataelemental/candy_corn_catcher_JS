d87cb6d9d7003e9d0b4c1e227c7121292a26647d
Data <95385516+thedataelemental@users.noreply.github.com>
2021-12-13 17:35:01 -0800

    Add files via upload

---
+++ candy_corn_catcher_JS.js
@@ -0,0 +1,31 @@
+// candy_corn_catcher_JS.js
+// Port of original arcade-style game "Candy Corn Catcher" from Python,
+// to JavaScript and HTML.
+// Author: Jackie P, aka TheDataElemental.
+
+
+// Player sprite with array of images and x & y coordinates
+class Player {
+	constructor(images, x_start, y_start){
+		this.images = images;
+		this.x_start = x_start;
+		this.y_start = y_start;
+	}
+}
+
+// Falling candy corns that player tries to catch
+class Candy {
+	constructor(image, x, y){
+		this.image = image
+		this.x = x
+		this.y = y
+	}
+}
+
+// HTML stuff... (instead of pygame init)
+
+// Import assets
+<img src="Assets/Exports/starry_night.png" alt="Night Sky Image">
+
+
+// Under construction...
